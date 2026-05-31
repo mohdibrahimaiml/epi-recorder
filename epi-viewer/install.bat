9b72da8eab2d165dea0c330677e98a9ff1d3ee00
Mohd Ibrahim A. <106313018+mohdibrahimai@users.noreply.github.com>
2026-06-01 00:11:54 +0530

    Remove deprecated Electron viewer (broken by polyglot envelope format)

--- epi-viewer/install.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-REM Add Node.js to PATH and run npm install
-SET "PATH=C:\Program Files\nodejs;%PATH%"
-npm install
-
- 
--- epi-viewer/test-electron.js
+++
@@ -1,7 +0,0 @@
-// Minimal test to check electron
-const electron = require('electron');
-console.log('electron object:', Object.keys(electron));
-console.log('app:', electron.app);
-
-
- 
